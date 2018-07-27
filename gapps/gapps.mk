b683d64b33f6cf2b78ac659053d68877ff2fa4f6
David <Davidteri91@gmail.com>
2018-07-27 22:09:29 +0200

    gapps: add gapps config
    Signed-off-by: David Viteri <davidteri91@gmail.com>

---
+++ gapps/gapps.mk
@@ -0,0 +1,63 @@
+# Copyright (C) 2016 The Sony AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Google property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.atrace.core.services=com.google.android.gms,com.google.android.gms.ui,com.google.android.gms.persistent \
+    ro.setupwizard.rotation_locked=true
+
+# Google Assistant
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opa.eligible_device=true
+
+# Audio (Notifications/Alarms)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.notification_sound=Duet.ogg \
+    ro.config.alarm_alert=Sunshower.ogg
+
+# OpenGapps
+GAPPS_VARIANT := micro
+GAPPS_FORCE_PACKAGE_OVERRIDES := true
+GAPPS_FORCE_WEBVIEW_OVERRIDES := true
+GAPPS_FORCE_BROWSER_OVERRIDES := true
+GAPPS_FORCE_PIXEL_LAUNCHER := true
+
+# Telephony Packages (Not windy devices allowed)
+ifneq ($(filter-out aosp_g1109, $(TARGET_PRODUCT)),)
+GAPPS_FORCE_DIALER_OVERRIDES := true
+GAPPS_FORCE_MMS_OVERRIDES := true
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.config.ringtone=The_big_adventure.ogg
+endif
+
+# Add some extras not in micro
+# To override stock AOSP apps
+PRODUCT_PACKAGES += \
+    CalculatorGoogle \
+    GoogleContacts \
+    GoogleExtServices \
+    GoogleExtShared \
+    GooglePrintRecommendationService \
+    LatinImeGoogle \
+    Music2 \
+    Photos \
+    PrebuiltDeskClockGoogle \
+    TagGoogle
+
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
